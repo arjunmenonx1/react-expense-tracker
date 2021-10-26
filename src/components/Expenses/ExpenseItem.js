f71f7bb0d92bacd0a298af9f10c18d36233b0e58
Arjun Menon <arjunmenon@Arjuns-MacBook-Pro.local>
2021-10-26 21:44:42 +0530

    handling form submission

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+  const [title, SetTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
+
+  const clickHandler = () => {
+    SetTitle("Updated!");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+      </div>
+      <div className="expense-item__price">{props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
